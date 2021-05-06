c437ae91c514a1e72a53d9926caf428726d41e53
lfga98 <lfgarciauaz@gmail.com>
2021-05-06 16:05:23 -0500

    Spring servlet

--- src/main/java/com/lfgaacademy/contactsapp/ContactsAppApplication.java
+++ src/main/java/com/lfgaacademy/contactsapp/ContactsAppApplication.java
@@ -2,13 +2,18 @@ package com.lfgaacademy.contactsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-public class ContactsAppApplication {
+public class ContactsAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsAppApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ContactsAppApplication.class);
+	}
 }
